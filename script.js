8335c4544896deeb822d2399f2dec6d93cd94f7c
Unnat Shah <unnatshah@gmail.com>
2023-05-24 23:01:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 /*script.js*/
 
-// Last Status
-const lastUpdatedElement = document.getElementById('last-updated');
-let lastUpdatedDate = new Date();
-
-function updateLastUpdatedStatus() {
-  const formattedDate = lastUpdatedDate.toLocaleString();
-  lastUpdatedElement.textContent = `Last Updated: ${formattedDate}`;
-}
-
-// Update last updated status when a user performs an action
-function updateUserAction() {
-  lastUpdatedDate = new Date(); // Update the last updated date
-  updateLastUpdatedStatus();
-}
-
-
 document.addEventListener('DOMContentLoaded', function() {
 
 //Color
@@ -54,96 +38,96 @@ var colorPreview = document.getElementById('color-preview');
 
 
   //initializing languages
-  var languages = {
-    'en': 'English',
-    'es': 'Spanish',
-    'fr': 'French',
-    'de': 'German',
-	'gu': 'Gujarati',
-	'hi': 'Hindi',
-    'it': 'Italian',
-    'ja': 'Japanese',
-    'ko': 'Korean',
-    'pt': 'Portuguese',
-    'ru': 'Russian',
-    'zh': 'Chinese',
-    // Add more language codes and names as needed
-  };
+var languages = {
+'en': 'English',
+'es': 'Spanish',
+'fr': 'French',
+'de': 'German',
+'gu': 'Gujarati',
+'hi': 'Hindi',
+'it': 'Italian',
+'ja': 'Japanese',
+'ko': 'Korean',
+'pt': 'Portuguese',
+'ru': 'Russian',
+'zh': 'Chinese',
+// Add more language codes and names as needed
+};
 
-  var languageSelect = document.getElementById('language-select');
-  var languageText = document.getElementById('language-text');
+var languageSelect = document.getElementById('language-select');
+var languageText = document.getElementById('language-text');
 
-  for (var langCode in languages) {
-    if (languages.hasOwnProperty(langCode)) {
-      var option = document.createElement('option');
-      option.value = langCode;
-      option.textContent = languages[langCode];
-      languageSelect.appendChild(option);
-    }
-  }
+for (var langCode in languages) {
+if (languages.hasOwnProperty(langCode)) {
+var option = document.createElement('option');
+option.value = langCode;
+option.textContent = languages[langCode];
+languageSelect.appendChild(option);
+}
+}
 
-  languageSelect.addEventListener('change', function() {
-    var selectedLanguage = languageSelect.value;
-    languageText.textContent = languages[selectedLanguage];
-  });
+languageSelect.addEventListener('change', function() {
+var selectedLanguage = languageSelect.value;
+languageText.textContent = languages[selectedLanguage];
+});
 
-  var storedLanguage = localStorage.getItem('languagePreference');
-  if (storedLanguage && languages.hasOwnProperty(storedLanguage)) {
-    languageSelect.value = storedLanguage;
-    languageText.textContent = languages[storedLanguage];
-  }
+var storedLanguage = localStorage.getItem('languagePreference');
+if (storedLanguage && languages.hasOwnProperty(storedLanguage)) {
+languageSelect.value = storedLanguage;
+languageText.textContent = languages[storedLanguage];
+}
 
 
-  // Update language text based on stored or selected language
-  updateLanguageText();
+// Update language text based on stored or selected language
+updateLanguageText();
 
-  // Event listener for language select
-  languageSelect.addEventListener('change', function() {
-    var selectedLanguage = languageSelect.value;
-    localStorage.setItem('languagePreference', selectedLanguage);
-    updateLanguageText();
-  });
+// Event listener for language select
+languageSelect.addEventListener('change', function() {
+var selectedLanguage = languageSelect.value;
+localStorage.setItem('languagePreference', selectedLanguage);
+updateLanguageText();
+});
 
-  function updateLanguageText() {
-    var selectedLanguage = languageSelect.value;
-    var translations = {
-      en: 'Default language text',
-      fr: 'Texte par défaut',
-      es: 'Texto predeterminado'
-      // Add more translations for each language as needed
-    };
+function updateLanguageText() {
+var selectedLanguage = languageSelect.value;
+var translations = {
+en: 'Default language text',
+fr: 'Texte par défaut',
+es: 'Texto predeterminado'
+// Add more translations for each language as needed
+};
 
-    // Check if translation exists in the translations object
-    if (translations[selectedLanguage]) {
-      languageText.textContent = translations[selectedLanguage];
-    } else {
-      // If translation is not available, use the default text
-      languageText.textContent = translations['en'];
-    }
+// Check if translation exists in the translations object
+if (translations[selectedLanguage]) {
+languageText.textContent = translations[selectedLanguage];
+} else {
+// If translation is not available, use the default text
+languageText.textContent = translations['en'];
+}
 
-    // Translate text if the selected language is not English
-    if (selectedLanguage !== 'en') {
-      translateText(languageText.textContent, selectedLanguage, function(translation) {
-        languageText.textContent = translation;
-      });
-    }
-  }
+// Translate text if the selected language is not English
+if (selectedLanguage !== 'en') {
+translateText(languageText.textContent, selectedLanguage, function(translation) {
+languageText.textContent = translation;
+});
+}
+}
 
-  function translateText(text, targetLanguage, callback) {
-    var apiKey = 'YOUR_API_KEY'; // Replace with your Microsoft Translator Text API key
+function translateText(text, targetLanguage, callback) {
+var apiKey = 'YOUR_API_KEY'; // Replace with your Microsoft Translator Text API key
 
-    Microsoft.Translator.translate({
-      text: text,
-      to: targetLanguage,
-      api_key: apiKey
-    }, function(result) {
-      if (result && result.translation) {
-        callback(result.translation);
-      } else {
-        console.error('Translation error:', result);
-        callback(text);
-      }
-    });
-  }
+Microsoft.Translator.translate({
+text: text,
+to: targetLanguage,
+api_key: apiKey
+}, function(result) {
+if (result && result.translation) {
+callback(result.translation);
+} else {
+console.error('Translation error:', result);
+callback(text);
+}
+});
+}
 
 });
